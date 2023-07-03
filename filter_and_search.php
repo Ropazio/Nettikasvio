37ad0e0dbbcb94ac6cdbea7ae39dfc2a677be3de
Riina Hemmilä <riinah97@gmail.com>
2023-07-03 23:40:07 +0300

    fixed dropdown and arrow

--- filter_and_search.php
+++ filter_and_search.php
@@ -2,29 +2,31 @@
 
 function get_filter() {
 
-	echo '<div class="filter">';
-	echo 	'<table class="filter_table">';
-	echo 		'<tr>';
-	echo 			'<th class="filter_table_headline filter_headline_border">';
-	echo 				'<form action="search.php">';
-  	echo 					'<input type="text" id="search" name="fname">';
-  	echo 					'<input type="submit" id="search_button" value="🔍">';
-	echo 				'</form>';
-	echo 			'</th>';
-	echo 			'<th class="filter_table_headline">';
-	echo 				'<h3>';
-	echo 					'<button class="dropdown" onclick="activate_dropdown()">Kukan väri';
-    echo 						'<i class="arrow_right"></i>';
-  	echo 					'</button>';
-  	echo 					'<div class="dropdown_content" id="colour_dropdown"</a>';
-    echo 						'<a class="colour_option" style="color:red" href="#">Punainen</a>';
-    echo 						'<a class="colour_option" style="color:blue" href="#">Sininen</a>';
-    echo 						'<a class="colour_option" style="color:yellow" href="#">Keltainen</a>';
-  	echo 					'</div>';
-	echo				'</h3>';
-	echo 			'</th>';
-	echo 		'</tr>';
-	echo 	'</table>';
-	echo '</div>';
+    echo '<div class="filter">';
+    echo    '<table class="filter_table">';
+    echo        '<tr>';
+    echo            '<form action="search.php">';
+    echo            '<th class="filter_table_search filter_headline_border">';
+    echo                '<input type="text" id="search">';
+    echo            '</th>';
+    echo            '<th class=filter_table_dropdown>';
+    echo                '<button type="button" class="dropdown" onclick="activate_dropdown()">Kukan väri';
+    echo                    '<i id="turn_arrow" class="arrow_right"></i>';
+    echo                '</button>';
+    echo                '<div>';
+    echo                    '<div class="dropdown_content" id="colour_dropdown"</a>';
+    echo                        '<a class="colour_option" style="color:red" href="#">Punainen</a>';
+    echo                        '<a class="colour_option" style="color:blue" href="#">Sininen</a>';
+    echo                        '<a class="colour_option" style="color:yellow" href="#">Keltainen</a>';
+    echo                    '</div>';
+    echo                '</div>';
+    echo            '</th>';
+    echo            '<th class="filter_table_button">';
+    echo                '<input type="submit" id="search_button">';
+    echo            '</th>';
+    echo            '</form>';
+    echo        '</tr>';
+    echo    '</table>';
+    echo '</div>';
 }
 ?>
--- operation.js
+++ operation.js
@@ -1,14 +1,16 @@
 var colour_dropdown = document.getElementById("colour_dropdown");
+var arrow_down = document.getElementById("turn_arrow");
 
 function activate_dropdown() {
 	colour_dropdown.classList.toggle("show_dropdown");
-	colour_dropdown.classList.toggle("arrow_down");
+	arrow_down.classList.toggle("turn_arrow");
 }
 
 window.onclick = function(misclick) {
 	if (!misclick.target.matches(".dropdown")) {
 		if (colour_dropdown.classList.contains("show_dropdown")) {
 			colour_dropdown.classList.remove("show_dropdown");
+			arrow_down.classList.remove("turn_arrow");
 		}
 	}
 }
